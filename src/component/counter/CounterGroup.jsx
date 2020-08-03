ef634138da975459c8bcb0244adb11775db2ecc7
GAOGA4 <961977096@qq.com>
2020-08-03 16:17:27 +0800

    [Gavin] feature: complete CounterGroup with specified number input

--- src/component/counter/CounterGroup.jsx
+++ src/component/counter/CounterGroup.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Counter from './Counter'
 class CounterGroup extends React.Component{
+    constructor(props){
+		super(props);
+		this.state = {
+			inpValu:1
+		}
+	}
+	
+	handelChange(e){
+        console.log(typeof e.target.value)
+		this.setState({
+			inpValu:e.target.value
+        })
+        
+	}
     render(){
         return(
             <div>
-                {new Array(6).fill(0).map((value,index) => <Counter key={index}/>)}
+            <input type="text" onChange={this.handelChange.bind(this)} defaultValue={this.state.inpValu}/>
+                <div >
+                    {new Array(Number(this.state.inpValu)).fill(0).map((value,index) => <Counter key={index}/>)}
+                </div>
             </div>
         ) 
     }
