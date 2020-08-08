19fee4787a4fdc4d5a349f16cde978cf5a5e5f29
GAOGA4 <961977096@qq.com>
2020-08-08 10:28:47 +0800

    [Gavin] rewrite: remove all and rewrite react-counter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers/index'
-import CounterGroupContainer from './containers/counterGroupContainer';
+
 
 const store = createStore(reducers)
 ReactDOM.render(
-    <Provider store={store}>
-        <CounterGroupContainer></CounterGroupContainer>
-    </Provider>
+    <React.StrictMode>
+    </React.StrictMode>
     , document.getElementById('root')
 );
